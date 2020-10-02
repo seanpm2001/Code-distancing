60680d0ec4c41575d28c738fdd40bffb5727170c
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 11:58:13 -0700

    CodeDistancingV1.cpp
    An archive of the first version of the C++ code distancing file.

---
+++ Code-distancing/C++/OldVersions/CodeDistancingV1.cpp
@@ -0,0 +1,47 @@
+/ Code distancing for Java
+
+
+
+
+
+
+#include <iostream>
+
+
+
+
+
+
+int main()
+
+
+
+
+
+
+{
+
+
+
+
+
+
+      std::cout << "Keep your Java code safe from COVID-19 by staying 6 line breaks apart";
+
+
+
+
+
+
+
+}  
+
+
+
+
+
+
+
+
+
+// Stay safe!
