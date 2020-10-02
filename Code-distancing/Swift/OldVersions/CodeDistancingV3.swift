8ed95efbece8a38d3b443d7a89200894a6d15551
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:22:08 -0700

    CodeDistancingV3.swift
    An archive of the 3rd version of the Swift code distancing file.

---
+++ Code-distancing/Swift/OldVersions/CodeDistancingV3.swift
@@ -0,0 +1,22 @@
+// Code distancing for Swift
+
+
+
+
+
+
+msg1 str = "Keep your Swift code safe from COVID-19 by staying 6 line breaks apart";
+
+
+
+
+
+
+print(msg1); // I can't find how to output in Swift.
+    
+    
+    
+  
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
