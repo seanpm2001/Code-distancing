3d36e5d9a99f6ae46fb92cf2519f00b86ab6d143
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:11:34 -0700

    CodeDistancingV1.swift
    An archive of the first version of the Swift code distancing file.

---
+++ Code-distancing/Swift/OldVersions/CodeDistancingV1.swift
@@ -0,0 +1,21 @@
+// Code distancing for Swift
+
+
+
+
+
+msg1 str = "Keep your Java code safe from COVID-19 by staying 6 line breaks apart";
+
+
+
+
+
+
+print(msg1); // I can't find how to output in Swift.
+    
+
+
+
+
+
+// Stay safe!
