c5fcc3d606d2ea09bc281590b106d41aa7b744a8
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:09:34 -0700

    CodeDistancingV2.py
    An archive of the 2nd version of the Python code distancing file.

---
+++ Code-distancing/Python3/OldVersions/CodeDistancingV2.py
@@ -0,0 +1,17 @@
+# Code distancing for Python 3
+
+
+
+
+
+
+print ("Keep your Python 3 code safe from COVID-19 by staying 6 line breaks apart")
+
+
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
