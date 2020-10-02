1117f53383f987b56df84c0964c558d302a23055
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:02:48 -0700

    CodeDistancingV2.java
    An archive of the 2nd version of the code distancing Java file.

---
+++ Code-distancing/Java/OldVersions/CodeDistancingV2.java
@@ -0,0 +1,43 @@
+// Code distancing for Java
+
+
+
+
+
+
+public class CodeDistancingApp {
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+    
+
+
+
+
+
+      System.out.println("Keep your Java code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
