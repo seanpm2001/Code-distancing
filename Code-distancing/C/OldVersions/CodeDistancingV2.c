0a13181ce33b54803078a867cab54a065f94aab0
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:00:59 -0700

    CodeDistancingV2.c
    An archive of the 2nd version of the C code distancing file.

---
+++ Code-distancing/C/OldVersions/CodeDistancingV2.c
@@ -0,0 +1,43 @@
+// Code distancing for C
+
+
+
+
+
+
+# include <stdio.h>
+
+
+
+
+
+
+int main(void)
+
+
+
+
+
+{
+
+
+
+
+
+
+      printf("Keep your C code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+
+}
+    
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
