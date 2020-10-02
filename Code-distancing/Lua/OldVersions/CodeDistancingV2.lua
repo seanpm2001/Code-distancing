6ee30e97b5985665be7228d34642b980ef992154
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 12:08:06 -0700

    CodeDistancingV2.lua
    An archive of the 2nd version of the Lua code distancing file.

---
+++ Code-distancing/Lua/OldVersions/CodeDistancingV2.lua
@@ -0,0 +1,17 @@
+-- Code distancing for Lua
+
+
+
+
+
+
+print 'Keep your Lua code safe from COVID-19 by staying 6 line breaks apart'
+
+
+
+
+
+
+
+
+--Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
