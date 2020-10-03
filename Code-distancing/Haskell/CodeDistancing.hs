557d3cfe163a5c69757d404fd727ff997ef20c3c
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-02 20:12:36 -0700

    CodeDistancing.hs
    Code Distancing in Haskell.

---
+++ Code-distancing/Haskell/CodeDistancing.hs
@@ -0,0 +1,29 @@
+module Main (main) where          -- not needed in interpreter, is the default in a module file
+
+
+
+
+
+
+main :: IO ()                     -- the compiler can infer this type definition
+
+
+
+
+
+
+-- Code distancing for Haskell
+
+
+
+
+
+
+main = putStrLn "Keep your Haskell code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+-- Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
