6a77f2cb5f6e4c2fe07e92204153eb88e3412d33
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-04 18:36:55 -0700

    CodeDistancingV1.cs
    An archive of the first version of the C# code distancing file.

---
+++ Code-distancing/C#/OldVersions/CodeDistancingV1.cs
@@ -0,0 +1,64 @@
+// Code distancing for C#
+
+
+
+
+
+
+using System;
+
+
+
+
+
+
+class Program
+
+
+
+
+
+
+{
+
+    
+    
+    
+    
+    
+    public static void Main(string[] args)
+
+
+
+
+
+
+    {
+
+
+
+
+
+
+        Console.WriteLine("Keep your C# code safe from COVID-19 by staying 6 line breaks apart.");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
