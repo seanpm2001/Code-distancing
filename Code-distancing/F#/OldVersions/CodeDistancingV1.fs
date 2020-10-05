4ad8af0a09b312ce0daac17ef04fda1a2a5dd23b
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-04 18:40:52 -0700

    CodeDistancingV1.fs
    An archive of the first version of the F# code distancing file.

---
+++ Code-distancing/F#/OldVersions/CodeDistancingV1.fs
@@ -0,0 +1,15 @@
+// Code distancing for F#
+
+
+
+
+
+
+printfn "Keep your C# code safe from COVID-19 by staying 6 line breaks apart."
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
