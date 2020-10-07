04c1db0adf49f432d2dd0c1f06e3394ab57efad9
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 13:14:45 -0700

    CodeDistancingV1.pl
    An archive of the first version of the Perl code distancing script file.

---
+++ Code-distancing/Perl/OldVersions/CodeDistancingV1.pl
@@ -0,0 +1,15 @@
+# Code distancing for Perl
+
+
+
+
+
+
+print "Keep your Perl code safe from COVID-19 by staying 6 line breaks apart\n";
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
