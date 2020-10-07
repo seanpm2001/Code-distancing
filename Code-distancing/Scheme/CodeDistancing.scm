03dabf0f15cb2144b601bfe5a81364bd2175e12f
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 13:06:16 -0700

    CodeDistancing.scm
    Code distancing for Scheme

---
+++ Code-distancing/Scheme/CodeDistancing.scm
@@ -0,0 +1,22 @@
+;; Code distancing for Scheme
+
+
+
+
+
+
+(let ((message0 (lambda() (display "Keep your Scheme code safe from COVID-19 by staying 6 line breaks apart") (newline))))
+  
+  
+  
+  
+  
+  
+  (message0))
+
+
+
+
+
+
+;; Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
