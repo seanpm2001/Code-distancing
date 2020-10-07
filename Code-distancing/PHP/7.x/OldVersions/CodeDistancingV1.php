c0a691a6f62895be7b4cda27f5960bce38baa423
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:12:45 -0700

    CodeDistancingV1.php
    An archive of the first version of the PHP 7 code distancing file.

---
+++ Code-distancing/PHP/7.x/OldVersions/CodeDistancingV1.php
@@ -0,0 +1,37 @@
+<?
+
+
+
+
+
+
+// Code distancing for PHP 7
+
+
+
+
+
+
+echo ("Keep your PHP 7 code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+
+print ("Keep your PHP 7 code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
+
+
+
+
+
+
+?>
