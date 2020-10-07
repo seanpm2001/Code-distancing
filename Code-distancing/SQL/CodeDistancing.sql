ab47ee2b020fb8ec79adc27c16c5a70dbc433239
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 13:02:08 -0700

    CodeDistancing.sql
    Code distancing for SQL.

---
+++ Code-distancing/SQL/CodeDistancing.sql
@@ -0,0 +1,37 @@
+--  Code distancing for SQL
+
+
+
+
+
+
+CREATE TABLE MESSAGE;
+
+
+
+
+
+
+
+INSERT INTO MESSAGE "Keep your SQL code safe from COVID-19 by staying 6 line breaks apart";
+
+
+
+
+
+
+COMMIT;
+
+
+
+
+
+
+DROP TABLE MESSAGE;
+
+
+
+
+
+
+-- Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
