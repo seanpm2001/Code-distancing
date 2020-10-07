41c8dc682dfd0032428d4294b6d90e8ca5159131
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 12:02:29 -0700

    CodeDistancing.rs
    Code distancing for Rust.

---
+++ Code-distancing/Rust/CodeDistancing.rs
@@ -0,0 +1,29 @@
+// Code distancing for Rust
+
+
+
+
+
+
+fn main() {
+    
+    
+    
+    
+    
+    
+    println!("Keep your Rust code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+}
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
