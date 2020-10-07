85f100199daac3f204b2939784bc433a909279db
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:19:15 -0700

    CodeDistancingV1.erl
    An archive of the first version of the code distancing Erlang script.

---
+++ Code-distancing/Erlang/OldVersions/CodeDistancingV1.erl
@@ -0,0 +1,15 @@
+%% Code distancing for Erlang
+
+
+
+
+
+
+console.log("Keep your Erlang code safe from COVID-19 by staying 6 line breaks apart"); %% Bad syntax, I can't find good documentation on output in Erlang
+
+
+
+
+
+
+%% Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
