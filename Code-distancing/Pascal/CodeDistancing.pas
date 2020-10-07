2018274de00f36f3001858fa2008f7426f0e3187
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 12:22:00 -0700

    CodeDistancing.pas
    Code distancing for Pascal.

---
+++ Code-distancing/Pascal/CodeDistancing.pas
@@ -0,0 +1,50 @@
+// Code distancing for Pascal
+
+
+
+
+
+
+program CodeDistancing(output);
+
+
+
+
+
+
+begin
+
+
+
+
+
+
+    Write('Keep your Pascal code safe from COVID-19 by staying 6 line breaks apart')
+    
+    
+    
+    
+    
+    
+    {No ";" is required after the last statement of a block -
+        
+        
+        
+        
+        
+        
+        adding one adds a "null statement" to the program, which is ignored by the compiler.}
+
+
+
+
+
+
+end.
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
