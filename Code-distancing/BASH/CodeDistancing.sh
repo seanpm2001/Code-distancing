9d3422d869361deef98639efe0304f4c4f06d2b3
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 13:19:15 -0700

    CodeDistancing.sh
    Code distancing for the Bourne Again Shell

---
+++ Code-distancing/BASH/CodeDistancing.sh
@@ -0,0 +1,31 @@
+# Code distancing for Bourne Again Shell
+
+
+
+
+
+
+$ # A traditional shell does not produce the same output
+
+
+
+
+
+
+$ #/bin/sh
+
+
+
+
+
+
+$ echo ("Keep your Python 3 code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
