942a2f6d9df7ac0523278044cdcd7b9dbf42b5db
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 14:15:35 -0700

    CodeDistancing.mm
    Code distancing for Objective-C.

---
+++ Code-distancing/Objective-C/CodeDistancing.mm
@@ -0,0 +1,57 @@
+// Code distancing for Objective-C
+
+
+
+
+
+
+// FILE: CodeDistancing.mm
+
+
+
+
+
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+
+int main (int argc, const char * argv[])
+
+
+
+
+
+
+{
+
+
+
+
+
+
+    NSLog(@"Keep your Objective-C code safe from COVID-19 by staying 6 line breaks apart\n");
+    
+    
+    
+    
+    
+    
+    return 0;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
