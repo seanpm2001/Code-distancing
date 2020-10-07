657255e984500a23d06531d853eb8be937c46410
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:13:56 -0700

    CodeDistancing.js
    Code distancing for JavaScript 1.8

---
+++ Code-distancing/JavaScript/1.8/CodeDistancing.js
@@ -0,0 +1,15 @@
+// Code distancing for JavaScript 1.8
+
+
+
+
+
+
+console.log("Keep your JavaScript 1.8 code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
