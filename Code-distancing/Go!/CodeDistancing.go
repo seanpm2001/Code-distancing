6a4fb34c6b19646641575eff4c33986b4924530d
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:23:54 -0700

    CodeDistancing.go
    Code distancing for the original Go! programming language.

---
+++ Code-distancing/Go!/CodeDistancing.go
@@ -0,0 +1,15 @@
+// Code distancing for Go!
+
+
+
+
+
+
+print ("Keep your Go! code safe from COVID-19 by staying 6 line breaks apart"); // The original Go! language has very poor documentation, so this is the best I can do.
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
