10f9b868e25df4c721125fcd8497f03fbfbf93ad
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:26:25 -0700

    CodeDistancing.go
    Code Distancing for Go.

---
+++ Code-distancing/Go (By Google, 2009)/CodeDistancing.go
@@ -0,0 +1,44 @@
+// Code distancing for Go (2009 by Google, not the original, from 2003)
+
+
+
+
+
+
+package main
+
+
+
+
+
+
+import "fmt"
+
+
+
+
+
+
+func main() {
+
+  
+  
+  
+  
+  
+  fmt.Println ("Keep your Go! code safe from COVID-19 by staying 6 line breaks apart");
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
