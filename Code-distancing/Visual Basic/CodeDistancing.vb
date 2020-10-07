12a21d81d00e28755d4717acc7f8a85885ec3a8b
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 13:11:22 -0700

    CodeDistancing.vb
    Code distancing for Visual Basic.

---
+++ Code-distancing/Visual Basic/CodeDistancing.vb
@@ -0,0 +1,37 @@
+' Code distancing for Visual Basic
+
+
+
+
+
+
+Private Sub Form_Load()
+
+
+
+
+
+
+    ' Execute a simple message box that says "Hello, World!"
+    
+    
+    
+    
+    
+    
+    MsgBox "Keep your Visual Basic code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+End Sub
+
+
+
+
+
+
+
+' Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
