8290a809af3e50bfb72c6c80034b185b06efa050
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 12:35:36 -0700

    CodeDistancing.f90
    Code distancing for Fortran 90.

---
+++ Code-distancing/FORTRAN/90/CodeDistancing.f90
@@ -0,0 +1,29 @@
+! Code distancing for FORTRAN 90
+
+
+
+
+
+
+program codedistancing
+     
+     
+     
+     
+     
+     
+     print *, "Keep your FORTRAN 90 code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+end program codedistancing
+
+
+
+
+
+
+! Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
