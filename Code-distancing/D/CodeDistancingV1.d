9c8e9bd9abf2ed771e2f5396efc78b8adb9a0713
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-07 11:35:16 -0700

    CodeDistancingV1.d
    An archive of the first version of the D programming language code distancing file.

---
+++ Code-distancing/D/CodeDistancingV1.d
@@ -0,0 +1,64 @@
+// Code distancing for D
+
+
+
+
+
+
+import std.stdio;
+
+
+
+
+
+
+void main() {
+    
+    
+    
+    
+    
+    
+    int multiplier = 10;
+    
+    
+    
+    
+    
+    
+    int scaled(int x) { return x * multiplier; }
+
+    
+    
+    
+    
+    
+    foreach (i; 0 .. 10) {
+        
+        
+        
+        
+        
+        
+        writefln("Keep your D code safe from COVID-19 by staying 6 line breaks apart %d! scaled = %d", i, scaled(i));
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+// Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
