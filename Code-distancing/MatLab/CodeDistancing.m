b37b306e47e940bf5815e37e9f70e27b44ab96ec
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 12:35:02 -0700

    CodeDistancing.m
    Code distancing in MatLab.

---
+++ Code-distancing/MatLab/CodeDistancing.m
@@ -0,0 +1,57 @@
+%% Code distancing for MatLab
+
+
+
+
+
+
+classdef CodeDistance
+    
+	
+	
+	
+	
+	
+	methods
+        
+		
+		
+		
+		
+		
+		function greet(obj)
+            
+			
+			
+			
+			
+			
+			disp('Keep your Matlab code safe from COVID-19 by staying 6 line breaks apart')
+        
+		
+		
+		
+		
+		
+		end
+    
+	
+	
+	
+	
+	
+	end
+
+
+
+
+
+
+end
+
+
+
+
+
+
+%% Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
