1173a80d22f38119e59310a4a734ef3b8fc36889
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 13:21:13 -0700

    CodeDistancing.ps1
    Code distancing for PowerShell.

---
+++ Code-distancing/PowerShell/CodeDistancing.ps1
@@ -0,0 +1,22 @@
+# Code distancing for PowerShell
+
+
+
+
+
+
+write-Host "Keep your PowerShell code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+echo "Keep your PowerShell code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
