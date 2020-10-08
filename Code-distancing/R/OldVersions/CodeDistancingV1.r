2f0f72b7f06ad9321ee217cec86d561adb676e0f
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 13:25:09 -0700

    CodeDistancingV1.r
    An archive of the first version of the R code distancing script file.

---
+++ Code-distancing/R/OldVersions/CodeDistancingV1.r
@@ -0,0 +1,15 @@
+# Code distancing for R
+
+
+
+
+
+
+print ("Keep your R code safe from COVID-19 by staying 6 line breaks apart")
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
