59750aaa7eeabd7dc8c04ddba085d23c7b044d54
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 12:11:03 -0700

    CodeDistancingV1.bat
    An archive of the first version of the batch code distancing file.

---
+++ Code-distancing/Batch/OldVersions/CodeDistancingV1.bat
@@ -0,0 +1,43 @@
+REM Code distancing for Batch
+
+
+
+
+
+
+@echo off
+
+
+
+
+
+
+title code-distancing
+
+
+
+
+
+
+color 0b
+
+
+
+
+
+
+echo Keep your Batch code safe from COVID-19 by staying 6 line breaks apart
+
+
+
+
+
+
+pause
+
+
+
+
+
+
+REM Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
