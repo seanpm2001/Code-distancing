cbab287f8f2a358fd5e90d3eb226801fcb0146c4
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 12:20:49 -0700

    CodeDistancingV1.cmake
    An archive of the first version of the CMake code distancing file.

---
+++ Code-distancing/CMake/OldVersions/CodeDistancingV1.cmake
@@ -0,0 +1,17 @@
+# Code distancing for Cmake
+
+
+
+
+
+
+message "Keep your CMake code safe from COVID-19 by staying 6 line breaks apart";;
+
+
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this! *)
