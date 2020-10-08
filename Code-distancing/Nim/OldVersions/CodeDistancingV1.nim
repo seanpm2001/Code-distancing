99eb4aa1985348219c3f04e56fc893c8b131819b
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-08 12:38:36 -0700

    CodeDistancingV1.nim
    An archive of the first version of the Nim/Nimrod Code distancing script file.

---
+++ Code-distancing/Nim/OldVersions/CodeDistancingV1.nim
@@ -0,0 +1,17 @@
+# Code distancing for NimRod/Nim
+
+
+
+
+
+
+echo "Keep your NimRod/Nim code safe from COVID-19 by staying 6 line breaks apart"
+
+
+
+
+
+
+
+
+# Stay safe! Wear a mask, stay 6 feet apart. Flatten the curve. We can beat this!
