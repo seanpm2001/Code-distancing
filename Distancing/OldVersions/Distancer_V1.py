7b5dc7854d01cef8af1c43882eca2e9bc0997e5e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-03-03 20:23:37 -0800

    Distancer_V1.py
    An archive of the first version of the distancer script.

---
+++ Distancing/OldVersions/Distancer_V1.py
@@ -0,0 +1,246 @@
+# Start of script
+# Don't worry, all this code is already fully vaccinated against CODEVID-19 and no unvaccinated scripts are around, as they are in separate buildings/outside. It does not need to code distance here
+
+"""
+About
+The distancer is a script that makes all your software code distance. It adds 6 lines between every existing line.
+"""
+
+# Section A
+def AppleScriptDistancing():
+  # A script to distance AppleScript code
+  # Not yet available
+def AdaDistancing():
+  # A script to distance Ada code
+  # Not yet available
+def ActionScriptDistancing():
+  # A script to distance ActionScript code
+  # Not yet available
+def AWKDistancing():
+  # A script to distance AWK code
+  # Not yet available
+# Section B
+def BrightScriptDistancing():
+  # A script to distance BrightScript code
+  # Not yet available
+def BooDistancing():
+  # A script to distance Boo code
+  # Not yet available
+# Section C
+def CDistancing():
+  # A script to distance C code
+  # Not yet available
+def CPlusPlusDistancing():
+  # A script to distance C++ code
+  # Not yet available
+def CSharpDistancing():
+  # A script to distance C# code
+  # Not yet available
+def CMakeDistancing():
+  # A script to distance CMake code
+  # Not yet available
+def CAMLDistancing():
+  # A script to distance CaML code
+  # Not yet available
+def CSoundDistancing():
+  # A script to distance CSound/CSound Document code
+  # Not yet available
+def COBOLDistancing():
+  # A script to distance COBOL code
+  # Not yet available
+# Section D
+def DDistancing():
+  # A script to distance D code
+  # Not yet available
+def DartDistancing():
+  # A script to distance Dart code
+  # Not yet available
+# Section E
+def ErlangDistancing():
+  # A script to distance Erlang code
+  # Not yet available
+def EiffelDistancing():
+  # A script to distance Eiffel code
+  # Not yet available
+def ElixirDistancing():
+  # A script to distance Elixir code
+  # Not yet available
+# Section F
+def FortranDistancing():
+  # A script to distance Fortran code
+  # Not yet available
+def FSharpDistancing():
+  # A script to distance F# code
+  # Not yet available
+# Section G
+def Go2009Distancing():
+  # A script to distance Go (2009 Google Language) code
+  # Not yet available
+def Go2004Distancing():
+  # A script to distance GO (2004 language) code
+  # Not yet available
+def GenieDistancing():
+  # A script to distance Genie code
+  # Not yet available
+# Section H
+def HTMLDistancing():
+  # A script to distance HTML code
+  # Not yet available
+def HaskellDistancing():
+  # A script to distance Haskell code
+  # Not yet available
+# Section I
+def InnoSetupDistancing():
+  # A script to distance Inno Setup code
+  # Not yet available
+def IronPythonDistancing():
+  # A script to distance IronPython code
+  # Not yet available
+# Section J
+def JavaDistancing():
+  # A script to distance Java code
+  # Not yet available
+def JavaScriptDistancing():
+  # A script to distance JavaScript code
+  # Not yet available
+def JScriptDistancing():
+  # A script to distance JScript code
+  # Not yet available
+def JQueryDistancing():
+  # A script to distance JQuery code
+  # Not yet available
+def JSONDistancing():
+  # A script to distance JSON code
+  # Not yet available
+# Section K
+def KotlinDistancing():
+  # A script to distance Kotlin code
+  # Not yet available
+# Section L
+def LuaDistancing():
+  # A script to distance Lua code
+  # Not yet available
+def LispDistancing():
+  # A script to distance Lisp code
+  # Not yet available
+# Section M
+def MatLabDistancing():
+  # A script to distance MATLAB code
+  # Not yet available
+def MarkdownDistancing():
+  # A script to distance Markdown code
+  # Not yet available
+def MakefileDistancing():
+  # A script to distance Makefile code
+  # Not yet available
+# Section N
+def NimDistancing():
+  # A script to distance Nim code
+  # Not yet available
+# Section O
+def ObjectiveCDistancing():
+  # A script to distance Objective-C code
+  # Not yet available
+def ObjectiveJDistancing():
+  # A script to distance Objective-J code
+  # Not yet available
+def OberonDistancing():
+  # A script to distance Oberon code
+  # Not yet available
+# Section P
+def phpDistancing():
+  # A script to distance PHP code
+  # Not yet available
+def perlDistancing():
+  # A script to distance Perl code
+  # Not yet available
+def pythonDistancing():
+  # A script to distance Python code
+  # Not yet available
+def pascalDistancing():
+  # A script to distance Pascal code
+  # Not yet available
+def pythonDistancing():
+  # A script to distance Pascal code
+  # Not yet available
+def PostScriptDistancing():
+  # A script to distance PostScript code
+  # Not yet available
+def PowerShellDistancing():
+  # A script to distance PowerShell code
+  # Not yet available
+# Section Q
+def QSharpDistancing():
+  # A script to distance Q# code
+  # Not yet available
+def QMLDistancing():
+  # A script to distance QML code
+  # Not yet available
+# Section R
+def RDistancing():
+  # A script to distance R code
+  # Not yet available
+def REBOLDistancing():
+  # A script to distance REBOL code
+  # Not yet available
+def RakuDistancing():
+  # A script to distance Raku code
+  # Not yet available
+# Section S
+def SwiftDistancing():
+  # A script to distance Swift code
+  # Not yet available
+def SchemeDistancing():
+  # A script to distance Scheme code
+  # Not yet available
+def ScalaDistancing():
+  # A script to distance Scala code
+  # Not yet available
+# Section T
+def ToolCommandLanguageDistancing():
+  # A script to distance TCL code
+  # Not yet available
+def TypeScriptDistancing():
+  # A script to distance TypeScript code
+  # Not yet available
+# Section U
+
+# Section V
+def VHDLDistancing():
+  # A script to distance Virtual Hard Disk Language (VHDL) code
+  # Not yet available
+def VerilogDistancing():
+  # A script to distance Verilog code
+  # Not yet available
+def VisualBasicScriptDistancing():
+  # A script to distance VBS code
+  # Not yet available
+def VisualBasicDotNetDistancing():
+  # A script to distance VB.NET code
+  # Not yet available
+def ValaDistancing():
+  # A script to distance Vala code
+  # Not yet available
+# W section
+
+# X section
+def XMLDistancing():
+  # A script to distance XML code
+  # Not yet available
+# Y section
+def YAMLDistancing():
+  # A script to distance YAML code
+  # Not yet available
+# Z section
+
+"""selector line"""
+# Program execution scripting goes here
+
+# Divider
+"""
+File info
+File type: Python script file (*.py)
+File version: 1 (Wednesday, March 3rd 2021 at 8:23 pm)
+Line count (including blank lines and compiler line): 247
+"""
+# End of script
